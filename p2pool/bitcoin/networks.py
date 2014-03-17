4610172aac604588023e41b375e9aafa23633ac3
Wellenreiter01 <wellenreiter01@t-online.de>
2014-03-17 22:39:04 +0100

    added Feathercoin-Testnet info

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -200,6 +200,28 @@ nets = dict(
         DUST_THRESHOLD=0.03e8,
     ),
 
+    feathercoin_testnet=math.Object(
+        P2P_PREFIX='fcc1b7dc'.decode('hex'),
+        P2P_PORT=19336,
+        ADDRESS_VERSION=111,
+        RPC_PORT=19337,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'feathercoinaddress' in (yield bitcoind.rpc_help()) and
+             (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 200*100000000 >> (height + 1)//3360000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='FTC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Feathercoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Feathercoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.feathercoin'), 'feathercoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.feathercoin.com/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.feathercoin.com/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.feathercoin.com/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
+
 
 )
 for net_name, net in nets.iteritems():
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -140,6 +140,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-alt',
         VERSION_CHECK=lambda v: True,
     ),
+    feathercoin_testnet=math.Object(     
+        PARENT=networks.nets['feathercoin_testnet'],
+        SHARE_PERIOD=30, # seconds
+        CHAIN_LENGTH=60*60//10, # shares
+        REAL_CHAIN_LENGTH=60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=120, # blocks
+        IDENTIFIER='4665617468657221'.decode('hex'),
+        PREFIX='b131010ba6d4729a'.decode('hex'),
+        P2P_PORT=19340,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=19327,
+        BOOTSTRAP_ADDRS='pool.maeh.org pool2.maeh.org'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
